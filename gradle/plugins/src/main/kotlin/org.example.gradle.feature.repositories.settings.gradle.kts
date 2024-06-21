6c287a3f4190459f6c4878ee4a288d88f8e59685
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-06-21 10:03:45 +0200

    Tweak repositories setup

--- gradle/plugins/src/main/kotlin/org.example.gradle.feature.repositories.settings.gradle.kts
+++ gradle/plugins/src/main/kotlin/org.example.gradle.feature.repositories.settings.gradle.kts
@@ -1,8 +1,3 @@
-pluginManagement {
-    // Get community plugins from the Gradle Plugin Portal
-    repositories.gradlePluginPortal()
-}
-
 dependencyResolutionManagement {
     // Get components from Maven Central
     repositories.mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ pluginManagement {
     includeBuild("gradle/plugins")
 }
 plugins {
+    id("org.example.gradle.feature.repositories")
     id("org.example.gradle.report.develocity")
     id("org.example.gradle.feature.build-cache")
     id("org.example.gradle.feature.project-structure")
-    id("org.example.gradle.feature.repositories")
 }
 
 rootProject.name = "gradle-project-setup-howto"
