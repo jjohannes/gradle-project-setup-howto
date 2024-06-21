283a6ea86a12a353b10f18bb9dabe7391d80d101
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-06-21 08:28:21 +0200

    Format comment

--- gradle/plugins/src/main/kotlin/org.example.gradle.base.identity.gradle.kts
+++ gradle/plugins/src/main/kotlin/org.example.gradle.base.identity.gradle.kts
@@ -1,5 +1,5 @@
 plugins { id("org.gradle.base") }
 
-// Set the group required to refer to a Module "from outside". I.e., when it is published or used in
-// Included Builds.
+// Set the group required to refer to a Module "from outside".
+// I.e., when it is published or used in Included Builds.
 group = "org.example.product"
