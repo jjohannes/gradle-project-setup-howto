a88f28c1100501f3786642be1535118bcfadf918
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-06-23 15:34:40 +0200

    Cleaner plugin setup in 'aggregation' project

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
+    id("org.gradle.java")
+    id("org.example.gradle.base.lifecycle")
+    id("org.example.gradle.base.dependency-rules")
+    id("org.example.gradle.check.format-gradle")
     id("org.example.gradle.report.code-coverage")
     id("org.example.gradle.report.plugin-analysis")
     id("org.example.gradle.report.sbom")
     id("org.example.gradle.report.test")
 }
 
-dependencies {
-    implementation(projects.app)
-}
+dependencies { implementation(projects.app) }
