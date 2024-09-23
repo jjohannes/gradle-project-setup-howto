adff35d1495e62447c68cb1b21446dcddd9746e1
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-09-23 04:54:22 +0200

    Update Dependency Analysis Plugin to 2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
     id("org.example.gradle.base.lifecycle.root")
     id("org.example.gradle.check.format-gradle.root")
-    // https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1256
-    id("com.autonomousapps.dependency-analysis")
 }
