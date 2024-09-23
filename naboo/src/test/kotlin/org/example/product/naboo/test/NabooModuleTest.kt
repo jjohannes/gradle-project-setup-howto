28384eaa4d3cacb3a38cd8521560eff8e89fa8ad
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-09-23 05:38:33 +0200

    Convert example to Kotlin

---
+++ naboo/src/test/kotlin/org/example/product/naboo/test/NabooModuleTest.kt
@@ -0,0 +1,11 @@
+package org.example.product.naboo.test
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class NabooModuleTest {
+    @Test
+    fun testModule() {
+        Assertions.assertSame(5, 1 + 2 + 2)
+    }
+}
