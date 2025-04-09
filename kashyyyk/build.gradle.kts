8376e986f8b9b3b87295e4630be72d1daf421878
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-04-09 15:14:29 +0200

    Add 'module-info.java' files

--- kashyyyk/build.gradle.kts
+++ kashyyyk/build.gradle.kts
@@ -1,10 +1,1 @@
 plugins { id("org.example.gradle.component.library") }
-
-dependencies {
-    api(projects.naboo)
-    api(projects.tatooine)
-    implementation(projects.bespin)
-    implementation(projects.kamino)
-
-    testImplementation(libs.junit.jupiter.api)
-}
