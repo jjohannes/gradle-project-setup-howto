170557a2b0922772bee7b21c45fa7062a05c62af
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-04-23 23:54:27 +0200

    Remove Gradle files

--- tatooine/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins { id("org.example.gradle.component.library") }
-
-dependencies {
-    api("com.github.racc:typesafeconfig-guice")
-    implementation("org.apache.zookeeper:zookeeper")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-}
