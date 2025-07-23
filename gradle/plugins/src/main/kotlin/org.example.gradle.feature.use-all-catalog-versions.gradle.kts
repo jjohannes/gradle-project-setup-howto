fa2e7137d8c40adf6693bb10583fb763566e4740
Patrick Neulichedl <patrickneu@hotmail.de>
2025-07-23 07:52:36 +0200

    Fix version catalog comment (#132)
    the implementation was adjusted with commit:
    883c49200903f778ad41f91c23e0847d31b51394

--- gradle/plugins/src/main/kotlin/org.example.gradle.feature.use-all-catalog-versions.gradle.kts
+++ gradle/plugins/src/main/kotlin/org.example.gradle.feature.use-all-catalog-versions.gradle.kts
@@ -1,6 +1,6 @@
 plugins { id("org.gradle.java-platform") }
 
-// Allow upgrading/downgrading (transitive) versions via catalog by adding strict constraints
+// Allow upgrading (transitive) versions via catalog by adding constraints
 dependencies.constraints {
     val libs = versionCatalogs.named("libs")
     val catalogEntries = libs.libraryAliases.map { libs.findLibrary(it).get().get() }
