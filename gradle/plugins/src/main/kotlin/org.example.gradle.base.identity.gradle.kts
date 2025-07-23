6c391d932e6944017fda1d31f5dabd27364dcb8a
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-07-24 00:01:17 +0200

    Add Convention Plugins for Kotlin

--- gradle/plugins/src/main/kotlin/org.example.gradle.base.identity.gradle.kts
+++ gradle/plugins/src/main/kotlin/org.example.gradle.base.identity.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("org.gradle.base") }
 
 // Set the group required to refer to a Module "from outside".
 // I.e., when it is published or used in Included Builds.
-group = "org.example.product.java"
+group = "org.example.product.kotlin"
 
 // Set the version from 'version.txt'
 version = providers.fileContents(isolated.rootProject.projectDirectory.file("gradle/version.txt")).asText.getOrElse("")
