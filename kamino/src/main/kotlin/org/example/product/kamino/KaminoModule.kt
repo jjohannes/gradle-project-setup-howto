7ad8428436983ec4b6a4708bce22b7ab7170d369
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-07-24 00:01:22 +0200

    Add Convention Plugins for Spring Boot (Kotlin variant)

--- kamino/src/main/kotlin/org/example/product/kamino/KaminoModule.kt
+++ kamino/src/main/kotlin/org/example/product/kamino/KaminoModule.kt
@@ -1,14 +1,9 @@
 package org.example.product.kamino
 
 import org.example.product.coruscant.CoruscantModuleData
-import org.jboss.resteasy.plugins.guice.GuiceResteasyBootstrapServletContextListener
-import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider
-import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap
 
 /** I am published and therefore I have Javadoc! */
 class KaminoModule {
-    /** Bootstrap... */
-    var bootstrap: ResteasyBootstrap? = null
 
     /** Data... */
     var data: CoruscantModuleData? = null
@@ -19,10 +14,6 @@ class KaminoModule {
      * @return all the important Classes
      */
     fun info(): Array<Class<*>> {
-        return arrayOf(
-            ResteasyBootstrap::class.java,
-            ResteasyJackson2Provider::class.java,
-            GuiceResteasyBootstrapServletContextListener::class.java
-        )
+        return arrayOf()
     }
 }
