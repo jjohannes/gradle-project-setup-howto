8060a706782a9907924183ea6cd23132f64eee94
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-09-09 08:53:32 +0200

    Gradle 9.0.0

--- gradle/plugins/src/main/kotlin/org.example.gradle.report.sbom.gradle.kts
+++ gradle/plugins/src/main/kotlin/org.example.gradle.report.sbom.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 
 // Generate a Software Bill of Materials for the software product
 tasks.cyclonedxBom {
-    notCompatibleWithConfigurationCache("https://github.com/CycloneDX/cyclonedx-gradle-plugin/issues/193")
     includeConfigs.add(configurations.runtimeClasspath.name)
     destination = layout.buildDirectory.dir("reports/sbom").get().asFile
 }
