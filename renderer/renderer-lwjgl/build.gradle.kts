210b40aceb80615c3111dda6dbcc238af7ee3fed
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-12-07 23:50:31 +0100

    Remove runtime dependencies now provided transitively

--- renderer/renderer-lwjgl/build.gradle.kts
+++ renderer/renderer-lwjgl/build.gradle.kts
@@ -8,10 +8,6 @@ dependencies {
     implementation(libs.lwjgl.opengl)
     implementation(libs.lwjgl.stb)
     implementation(libs.slf4j.api)
-    runtimeOnly(libs.lwjgl) { capabilities { requireFeature("natives") } }
-    runtimeOnly(libs.lwjgl.glfw) { capabilities { requireFeature("natives") } }
-    runtimeOnly(libs.lwjgl.opengl) { capabilities { requireFeature("natives") } }
-    runtimeOnly(libs.lwjgl.stb) { capabilities { requireFeature("natives") } }
 
     testImplementation(libs.junit.api)
 }
